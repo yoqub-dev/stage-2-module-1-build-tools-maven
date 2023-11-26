148333718c77905dd9d87616888aad06d4273929
yoqub-dev <yoqub.epam@gmail.com>
2023-11-27 00:55:01 +0500

    teat

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,6 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        return StringUtils.isPositiveNumber(str) && Integer.parseInt(str) > 0;
     }
 }
